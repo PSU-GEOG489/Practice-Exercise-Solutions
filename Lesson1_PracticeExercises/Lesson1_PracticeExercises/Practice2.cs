9da4ffd616d6ce727940598aefae8331fe792885
Andrew Murdoch <ahm126@psu.edu>
2016-12-16 07:26:00 +0000

    first commit to new repo

---
+++ Lesson1_PracticeExercises/Lesson1_PracticeExercises/Practice2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+using System.Windows.Forms;
+
+namespace Lesson1_PracticeExercises
+{
+    public class Practice2 : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        public Practice2()
+        {
+        }
+
+        private void PracticeExercise2()
+        {
+            int x;
+            x = 9;
+            int y;
+            y = 7;
+            int z;
+            z = x + y;
+
+            MessageBox.Show(z.ToString(), "Practice 2", MessageBoxButtons.OK);
+        }
+
+        protected override void OnClick()
+        {
+            PracticeExercise2();
+        }
+
+
+        protected override void OnUpdate()
+        {
+        }
+    }
+}
---
+++ Lesson1_PracticeExercises/Lesson1_PracticeExercises/Practice3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+using Microsoft.VisualBasic;
+using System.Windows.Forms;
+
+namespace Lesson1_PracticeExercises
+{
+    public class Practice3 : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        public Practice3()
+        {
+        }
+
+        private void PracticeExercise3()
+        {
+            string x;
+            x = Interaction.InputBox("Please enter your name");
+            MessageBox.Show("Hi " + x + "!", "Practice 3", MessageBoxButtons.OK);
+        }
+
+        protected override void OnClick()
+        {
+            PracticeExercise3();
+        }
+
+        protected override void OnUpdate()
+        {
+        }
+    }
+}
+
